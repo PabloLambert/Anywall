6d0666d81808e8afbe313a4abf496f67d9953bef
Pablo Lambert <pablo.lambert@gmail.com>
2015-05-19 00:23:08 -0400

    Agrega Travel con Places y Data a Student.-

--- app/src/main/java/com/parse/anywall/StudentActivity.java
+++ app/src/main/java/com/parse/anywall/StudentActivity.java
@@ -1,32 +1,38 @@
 package com.parse.anywall;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.parse.DeleteCallback;
 import com.parse.GetCallback;
 import com.parse.ParseACL;
 import com.parse.ParseException;
-import com.parse.ParseGeoPoint;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * Activity which displays a login screen to the user, offering registration as well.
@@ -40,10 +46,17 @@ public class StudentActivity extends FragmentActivity {
   final static public String STUDENT_OBJECT_ID = "STUDENT_OBJECT";
   final static public int STUDENT_ACTION_CREATE = 1;
   final static public int STUDENT_ACTION_MODIFY = 2;
+  final static public Calendar c = Calendar.getInstance();
+  final static public int FROM_HOUR_DAY = c.get(Calendar.HOUR_OF_DAY);
+  final static public int FROM_MINUTES = c.get(Calendar.MINUTE);
 
   EditText textStudentName;
+  TextView textFromDate;
   Button btnStudentAction, btnStudentDelete;
-  ImageButton btnStudentInfo;
+  ImageButton btnStudentInfo, btnFromTimePicker;
+  Spinner spinnerFromTime;
+  int fromHourDay = FROM_HOUR_DAY, fromMinutes = FROM_MINUTES;
+  TimePickerFragment pickerFromTime;
   int action;
   Student student;
 
@@ -57,6 +70,9 @@ public class StudentActivity extends FragmentActivity {
     btnStudentAction = (Button) findViewById(R.id.btnStudentAction);
     btnStudentDelete = (Button) findViewById(R.id.btnStudentDelete);
     btnStudentInfo = (ImageButton) findViewById(R.id.btnStudentInfo);
+    spinnerFromTime = (Spinner) findViewById(R.id.spinnerFromPlace);
+    textFromDate = (TextView) findViewById(R.id.textFromDate);
+    btnFromTimePicker = (ImageButton) findViewById(R.id.btnFromTimePicker);
 
 
     textStudentName.addTextChangedListener(new TextWatcher() {
@@ -74,6 +90,26 @@ public class StudentActivity extends FragmentActivity {
         updateButtonState();
 
 
+      }
+    });
+
+    ArrayList<String> stringList = new ArrayList<String>();
+    for (Iterator<Places> iterator = MainActivity.mapPlaces.values().iterator(); iterator.hasNext(); ){
+      Places _p = iterator.next();
+      stringList.add(_p.getName());
+    }
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, stringList);
+    spinnerFromTime.setAdapter(adapter);
+
+    textFromDate.setText(showTime(FROM_HOUR_DAY, FROM_MINUTES));
+    btnFromTimePicker.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if ( pickerFromTime == null) {
+          pickerFromTime = new TimePickerFragment();
+        }
+        pickerFromTime.show(getSupportFragmentManager(), "timePicker");
       }
     });
 
@@ -130,6 +166,16 @@ public class StudentActivity extends FragmentActivity {
 
   }
 
+  public Places getPlaceFromMap(String _name) {
+
+    for (Iterator<Places> iterator = MainActivity.mapPlaces.values().iterator(); iterator.hasNext(); ){
+      Places _p = iterator.next();
+      if (_p.getName().compareTo(_name) == 0) return _p;
+    }
+    return null;
+  }
+
+
   public void updateButtonState() {
     int placeNameLength = textStudentName.getText().toString().length();
 
@@ -159,22 +205,20 @@ public class StudentActivity extends FragmentActivity {
     dialog.setMessage("Guardando Estudiante...");
     dialog.show();
 
+    ParseUser user = ParseUser.getCurrentUser();
+    ParseACL acl = new ParseACL(user);
+
     s.setName(name);
     ArrayList<Travel> travels = new ArrayList<Travel>();
     Travel t1 = new Travel();
-    t1.setName("Desde la casa");
+    t1.setFromPlace(getPlaceFromMap(spinnerFromTime.getSelectedItem().toString()));
+    t1.setFromHourOfDay(fromHourDay);
+    t1.setFromMinutes(fromMinutes);
+    t1.setACL(acl);
     travels.add(t1);
 
-    Travel t2 = new Travel();
-    t2.setName("Desde el colegio");
-    travels.add(t2);
-
     s.put("travel", travels);
-
-    ParseUser user = ParseUser.getCurrentUser();
-    s.setACL(new ParseACL(user));
-
-    // Save the Student
+    s.setACL(acl);
     s.saveInBackground(new SaveCallback() {
       @Override
       public void done(ParseException e) {
@@ -200,6 +244,49 @@ public class StudentActivity extends FragmentActivity {
     });
   }
 
+  public String showTime(int hour, int min) {
+    String format;
+
+    if (hour == 0) {
+      hour += 12;
+      format = "AM";
+    } else if (hour == 12) {
+      format = "PM";
+    } else if (hour > 12) {
+      hour -= 12;
+      format = "PM";
+    } else {
+      format = "AM";
+    }
+    StringBuilder sb = new StringBuilder().append(hour).append(" : ").append(min).append(" ").append(format);
+    return sb.toString();
+  }
+
+  public class TimePickerFragment extends DialogFragment
+          implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+      // Use the current time as the default values for the picker
+      int hour = FROM_HOUR_DAY;
+      int minute = FROM_MINUTES;
+
+      // Create a new instance of TimePickerDialog and return it
+      return new TimePickerDialog(getActivity(), this, hour, minute,
+              DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+      // Do something with the time chosen by the user
+
+      // set current time into textview
+      textFromDate.setText(showTime(hourOfDay, minute));
+      fromHourDay = hourOfDay;
+      fromMinutes = minute;
+
+    }
+
+  }
 
 }
 
--- app/src/main/java/com/parse/anywall/Travel.java
+++ app/src/main/java/com/parse/anywall/Travel.java
@@ -5,6 +5,8 @@ import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.Date;
+
 /**
  * Created by InnovaTI on 17-05-15.
  */
@@ -13,7 +15,7 @@ public class Travel extends ParseObject {
 
     public String getName() { return getString("name");}
 
-    public void setName(String value) { put("name", value); }
+    public void setName(String value) { put("name", value);}
 
         public Places getFromPlace() {
             return (Places) getParseObject("from_place");
@@ -22,6 +24,16 @@ public class Travel extends ParseObject {
         public void setFromPlace(Places value) {
             put("from_place", value);
         }
+
+    public int getFromHourOfDay() { return getInt("from_hourofday");}
+
+    public void setFromHourOfDay(int value) { put("from_hourofday", value);}
+
+
+    public int getFromMinutes() { return getInt("from_minutes");}
+
+    public void setFromMinutes(int value) { put("from_minutes", value);}
+
 
         public static ParseQuery<Travel> getQuery() {
             return ParseQuery.getQuery(Travel.class);
