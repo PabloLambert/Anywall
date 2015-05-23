eca79e705379bdbfe925641e5deece00451d62b9
Pablo Lambert <pablo.lambert@gmail.com>
2015-05-23 17:08:51 -0400

    WelcomeActivity

--- driver/src/main/java/com/lambertsoft/driver/DispatchActivity.java
+++ driver/src/main/java/com/lambertsoft/driver/DispatchActivity.java
@@ -1,38 +1,29 @@
 package com.lambertsoft.driver;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.ParseUser;
 
 public class DispatchActivity extends Activity {
+
+    public DispatchActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dispatch);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dispatch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Check if there is current user info
+        if (ParseUser.getCurrentUser() != null) {
+            // Start an intent for the logged in activity
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            // Start and intent for the logged out activity
+            startActivity(new Intent(this, WelcomeActivity.class));
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
